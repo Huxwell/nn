435ea06ce2ef8d1d61ed1abf65063cf8ebdd9cc4
Huxwell <filip.drapejkowski@gmail.com>
2015-10-20 14:59:57 +0200

    Sigmoid and iterating bugs fixed.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PIL import Image as Im
 rand = lambda: random.random() - 0.5  # from -0.5 to 0.5)
 sigmoid = lambda (x): 1.0 / (1.0 + np.exp(-x))
 grad_sigmoid = lambda (x): sigmoid(x) * (1 - sigmoid(x))
-signum = lambda (x): int(x >= 0)
+signum = lambda (x): 1 if x >= 0 else -1
 
 
 class NN(object):
@@ -32,10 +32,12 @@ class NN(object):
 
     def forward(self, dig_data, dig_no):
         self.clean_neurons()
+        # put a digit in input layer
         self.neurons[0] = dig_data[:]
         for l_id, layer in enumerate(self.neurons):
+            # if l_id = 4 - 1 = 3  so if we are in output layer
             if l_id == self.neurons.__len__() - 1:
-                return [ signum(i * self.weights[l_id][no]) for no in self.neurons[3]]
+                return [signum(no * self.weights[l_id][no][0]) for no in self.neurons[3]]
             for idx, i in enumerate(self.neurons[l_id]):
                 for idx2, n in enumerate(self.neurons[l_id + 1]):
                     self.neurons[l_id + 1][idx2] += signum(i * self.weights[l_id][idx][idx2])
@@ -50,10 +52,10 @@ nn = NN(77, 10, 2)
 
 digits = ldigits();
 nn.forward(digits[8], 8)
-nn.forward(digits[8], 8)
+#nn.forward(digits[8], 8)
 
 
 print nn.forward(digits[8], 8)
 
-print nn.neurons[3]
+#print nn.neurons[3]
 
