82637674bfb34673820a39ff76051ea28c284f40
Huxwell <filip.drapejkowski@gmail.com>
2015-10-24 18:51:23 +0200

    attempt to build single hidden-layer net from scratch

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from mynn import *
-from cnn import *
-nn = NN(77, 10, 2, 0.2)
-digits = ldigits();
-nn.epoch(digits[8], 8)
-print nn.neurons[3]
-
+#from niceNN import *
+#from cnn import *
+#nn = NN(77, 10, 2, 0.2)
+#digits = ldigits();
+#nn.epoch(digits[8], 8)
 # print NeuralNetwork(77,10,10,2)
+from singleh import *
+nn = NN();
+
+print('\n'.join([''.join([' {:4}'.format(item) for item in row])
+      for row in nn.ws_o]))
+
+digs = ldigits()
+print len(nn.ws_h[0])
+nn.forward(digs[8],8)
