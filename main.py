ad06fd57eaf4e8b2c6f3199ab5f2349742a3bf98
Huxwell <filip.drapejkowski@gmail.com>
2015-10-25 00:13:40 +0200

    bugfixes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ nn = NN();
 # print('\n'.join([''.join([' {:4}'.format(item) for item in row])
 #      for row in nn.ws_o]))
 digs = ldigits()
-for x in xrange(100):
+for x in xrange(20):
       dig = int(random.random() * 10)
       nn.epoch(digs[dig],dig)
-print [round(f,1000) for f in nn.epoch(digs[0],0)]
+print [round(f,4) for f in nn.epoch(digs[7],7)]
--- singleh.py
+++ singleh.py
@@ -14,21 +14,19 @@ class NN(object):
 
     def __init__(self, n_i=77, n_o=10, n_h=44, learn=0.2):
         self.learn = learn
-        self.n_i = n_i
-        self.n_o = n_o
-        self.n_h = n_h
         self.ws_h = [[rand() for i in xrange(n_i)] for h in xrange(n_h)]
         self.ws_o = [[rand() for i in xrange(n_h)] for h in xrange(n_o)]
 
     def epoch(self, xes, y):
         hidd = [sum([x * w for x, w in izip(xes,n)]) for n in self.ws_h]
-        outs = [sum([sigmoid(h) * w for h, w in izip(hidd,n)]) for n in self.ws_o]
+        outs = [sigmoid(sum([sigmoid(h) * w for h, w in izip(hidd,n)])) for n in self.ws_o]
         prop_o = [1 if i==y else 0 for i in xrange(10)]
         err_o = [(proper_out - sigmoid(out)) * grad_sigmoid(out) for out, proper_out in izip(outs, prop_o)]
         ws_o_trans = zip(*self.ws_o)
         err_h = [grad_sigmoid(h) * sum([e * w for e, w in izip(err_o,ws_o_trans[h_idx])]) for h_idx, h in enumerate(hidd)]
-        self.ws_o = [[w * self.learn * e * sigmoid(net) for w, net in izip(w_n,hidd)] for w_n, e in izip(self.ws_o,err_o)]
-        self.ws_h = [[w * self.learn * e * sigmoid(x) for w, x in izip(w_n,xes)] for w_n, e in izip(self.ws_h,err_h)]
+        self.ws_o = [[w + self.learn * e * sigmoid(net) for w, net in izip(w_n,hidd)] for w_n, e in izip(self.ws_o,err_o)]
+        self.ws_h = [[w + self.learn * e * sigmoid(x) for w, x in izip(w_n,xes)] for w_n, e in izip(self.ws_h,err_h)]
+        print [round(f,2) for f in outs]
         return outs
 
 def ldigits():
